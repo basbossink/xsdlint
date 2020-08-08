572a79c25bf5434ee676863b279241868c4d6dbc
Bas Bossink <bas.bossink@gmail.com>
2020-08-09 00:00:20 +0200

    Make it pass

--- src/Validate.fs
+++ src/Validate.fs
@@ -2,4 +2,43 @@ module XsdLint.Validate
 
 open XsdLint.Types
 
-let validate (schema: Schema): ValidationMessage list = []
+let isMinOccursZero (attribute: AttributeLocation): Location option =
+    match attribute with
+    | { Attribute = (MinOccurs(Bounded 0u)); Location = l } -> (Some l)
+    | _ -> None
+
+let isOptional (attributes: Attributes): Location list = attributes |> List.choose isMinOccursZero
+
+let isNameEndingWithId (attribute: AttributeLocation): string option =
+    match attribute with
+    | { Attribute = (Name name); Location = l } when name.EndsWith
+                                                         ("Id", System.StringComparison.InvariantCultureIgnoreCase) ->
+        (Some name)
+    | _ -> None
+
+let nameEndsWithId (attributes: Attributes): string list = attributes |> List.choose isNameEndingWithId
+
+let validateFragments (fragemnts: Schema list) (messages: ValidationMessage list) = messages
+
+let rec validateStartElement
+        (startElement: ElementLocation * Attributes * Schema list)
+        (messages: ValidationMessage list)
+        : ValidationMessage list
+    =
+    match startElement with
+    | ({ Element = XsElement; Location = l }, attrs, rest) ->
+        match (nameEndsWithId attrs, isOptional attrs) with
+        | ([ name ], [ lopt ]) ->
+            validateFragments rest
+                (({ RuleIdentifier = RuleIdentifier.IdShouldBeRequired
+                    Location = lopt
+                    Severity = Severity.Warning
+                    Message = sprintf "Entity '%s' has a name ending with 'Id', hence it should be required." name })
+                 :: messages)
+        | _ -> validateFragments rest messages
+    | (_, _, rest) -> validateFragments rest messages
+
+let validate (schema: Schema): ValidationMessage list =
+    match schema with
+    | StarElement e -> validateStartElement e []
+    | _ -> []
--- test/Test.XsdLint/Tests.fs
+++ test/Test.XsdLint/Tests.fs
@@ -6,24 +6,28 @@ open XsdLint.Types
 open XsdLint.Validate
 
 [<Fact>]
-let ``Elements with a name ending with Id should be required test``() =
+let `` Elements with a name ending with Id should be required test``() =
     let input =
         (StarElement
             ({ Element = XsElement
                Location =
                    { Line = 37u
                      Column = 45u } },
-             [ { Attribute = (MinOccurs(Bounded 0u))
+             [ { Attribute = (Name "testId")
                  Location =
                      { Line = 37u
-                       Column = 48u } } ], []))
+                       Column = 48u } }
+               { Attribute = (MinOccurs(Bounded 0u))
+                 Location =
+                     { Line = 37u
+                       Column = 58u } } ], []))
     let expected =
         [ { Location =
                 { Line = 37u
-                  Column = 42u }
+                  Column = 58u }
             RuleIdentifier = RuleIdentifier.IdShouldBeRequired
-            Severity = Error
-            Message = "Entities that have a name ending with 'Id' should be requiered" } ]
+            Severity = Warning
+            Message = "Entity 'testId' has a name ending with 'Id', hence it should be required." } ]
 
     let actual = validate input
     actual |> should equal expected
